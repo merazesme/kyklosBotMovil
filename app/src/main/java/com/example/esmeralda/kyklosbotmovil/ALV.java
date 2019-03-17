a64280f56910ae276f7914d905a4d09536bc2d09
Esme Meraz <2016030013@gmail.com>
2019-03-17 11:20:32 -0700

    Comentario en clase ALV

--- app/src/main/java/com/example/esmeralda/kyklosbotmovil/ALV.java
+++ app/src/main/java/com/example/esmeralda/kyklosbotmovil/ALV.java
@@ -9,5 +9,6 @@ public class ALV extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alv);
+        //ALFIN ALVVVVVVVVVVV
     }
 }
