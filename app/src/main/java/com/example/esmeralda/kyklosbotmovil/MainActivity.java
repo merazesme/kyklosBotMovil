9b24ceae1387151230c04072f5092b3d84d6d83a
jesusVizcarra <jesus_cuate98@hotmail.es>
2019-04-03 10:31:00 -0700

    aaaaaaaa

--- app/src/main/java/com/example/esmeralda/kyklosbotmovil/MainActivity.java
+++ app/src/main/java/com/example/esmeralda/kyklosbotmovil/MainActivity.java
@@ -10,20 +10,15 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
-    CardView verCupones;
-    CardView misCupones;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_main);
 
 
         CardView verCupones = (CardView)findViewById(R.id.irVerCupones);
         CardView misCupones = (CardView)findViewById(R.id.irMisCupones);
         CardView info = (CardView)findViewById(R.id.irInformacion);
-
-        verCupones = (CardView)findViewById(R.id.irVerCupones);
-        misCupones = (CardView)findViewById(R.id.irMisCupones);
 
         verCupones.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,9 +34,18 @@ public class MainActivity extends AppCompatActivity {
                 Intent pantallaMisCupones = new Intent(getApplicationContext(), misCupones.class);
                 startActivity(pantallaMisCupones);
             }
-
         });
 
+        info.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent pantallaInformacion = new Intent(getApplicationContext(), howTo.class);
+                startActivity(pantallaInformacion);
+            }
+        });
+
+
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menuopciones, menu);
